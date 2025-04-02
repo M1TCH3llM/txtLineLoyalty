b8cc615411a70a84e992b26aa2fe3a192dfbbca9
Mitchell Morgan <mitchemorgan@gmail.com>
2025-04-02 15:30:43 -0600

    commenting code

--- loyalty_app.py
+++ loyalty_app.py
@@ -31,30 +31,46 @@ def save_loyalty(data):
 
 '''defining the functions to add points to the customer, reedem points from the customer, list the points of the customer, show the points of the customer, and the welcome message'''
 def add_points(data, name, points):
+    '''checking if the name is in the data'''
     if name in data:
+        '''adding the points to the existing points'''
         data[name] += points
+        '''printing the message'''
     else:
+        '''adding the points to the data'''
         data[name] = points
+        '''printing the message'''
         print(f"Added {points} points to {name}. New balance is {data[name]}")
 
+'''defining the function to reedem points from the customer'''
 def reedem_points(data, name, points):
-
+    '''checking if the name is in the data'''
     if name in data:
+        '''checking if the points are greater than or equal to the points'''
         if data[name] >= points:
+            '''deducting the points from the existing points'''
             data[name] -= points
+            '''printing the message'''
             print(f"Reedemed {points} points from {name}. New balance is {data[name]}")
+
         else:
+            '''printing the message if the points are not enough to reedem'''
             print(f"{name} does not have enough points to reedem")
     else: 
+         '''printing the message if name not found in data'''
          print(f"{name} not founbd in data base")
 
+'''defining the function to list the points of the customer'''
 def list_points(data):
-
+    '''checking if the data is empty'''
     if not data:
         print("No loyalty points available")
     else:
+        '''printing the loyalty points'''
         print("Loyalty points:")
+        '''iterating through the dictionary and printing the data'''
         for name, points in data.items():
+            '''printing the data'''
             print(f"{name}: {points}")
 
 def welcome_message():
@@ -78,28 +94,44 @@ def Help_message():
     print("  exit - Exits the application.")
 
 def show_points(data, name):
-    
+    '''checking if the name is in the data'''
     if name in data:
+        '''printing the points of the customer'''
         print(f"{name} has {data[name]} points")
     else:
+        '''printing the message if name not found in data'''
         print(f"{name} not found in database")
 
+'''defining the main function'''
 def main():
+    '''loading the loyalty points from the loyalty.txt file'''
+    '''calls the load_loyalty function'''
     data = load_loyalty()
 
+    '''running the loop until the user exits the application'''
     while True:
+        '''taking the input from the user'''
         command = input(">").strip().split()
-
+        '''checking if the command is empty'''
         if not command:
+            '''continuing the loop'''
             continue
+        '''getting the action from the command'''
         action = command[0].lower()
 
+        '''checking the action, adding points, reedem points, show points, list points, help, and exit'''
         if action == 'add':
+            '''checking the length of the command'''
             if len(command) == 3:
+                '''getting the name and points from the command'''
                 name = command[1]
+                '''checking if the points is integer'''
                 try:
+                    '''converting the points to integer'''
                     points = int(command[2])
+                    '''calling the add_points function'''
                     add_points(data, name, points)
+                    '''handling the exception'''
                 except ValueError:
                     print("Invalid points value")
             else:
